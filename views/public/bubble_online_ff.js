2670e1991698b4e1db31fe22ca3c573f24e692a0
Libinzi Ma <504321494@qq.com>
2020-04-09 10:15:50 +0800

    带有padding边框且居中的版本

--- views/public/bubble_online_ff.js
+++ views/public/bubble_online_ff.js
@@ -4131,8 +4131,6 @@
     let div3 = d3.select("#main").append("div")
       .attr("class", "tooltip3")
       .style("opacity", 0);
-    middleTitleSvg.append('i')
-      .attr('class','cil-info');
     middleTitleSvg
       .append("text")
       .attr("transform", "translate(" + 0 + " ," + 40 + ")")
